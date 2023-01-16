744a4ed2b94b3a258d5508b3f7e312f25036ea38
tod <ted.bergholtz@skola.taby.se>
2023-01-16 13:14:48 +0100

    commit69
    Co-authored-by: Sebastiano de Capre el Primo de lalandia y tu Marde <SebastianoDeCapre@users.noreply.github.com>

--- Gymden/Gymden.py
+++ Gymden/Gymden.py
@@ -1,5 +1,25 @@
 import random
 
+import time
+import os
+import sys
+
+
+def typingPrint(text):
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.01)
+
+
+def typingInput(text):
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.01)
+    value = input()
+    return value
+
 
 Planet_name = ["Bacu", "Ulu", "Html", "Ga",
                "Dagu", "kua", "los", "pollos", "hermanos"]
@@ -23,52 +43,59 @@ legendary_item = ["comically small revolver", "BFG 9000"]
 
 Mythical_item = ["comically large spoon"]
 
+# Inventory Function
+
+
 # Events for planet one
 
 # Event 1
 
-
 def Planet1_event1():
-    plan = input(
+    plan = typingInput(
         "As you are decending towards sed planetuas the ship is hot. \n What is your plan of action? \n[1] Eavasiv Manuvers \n[2] Override the engine \n[3] Shoot the control panel")
     if plan == "1":
         difficulty = random.randint(1, 20) + int(skill_pilot)
         if difficulty >= 15:
-            print("You succsesfully managed to control the ship into a safe decent and landed without any mayor issues")
+            typingPrint(
+                "You succsesfully managed to control the ship into a safe decent and landed without any mayor issues")
 
         elif difficulty >= 10:
-            print(
+            typingPrint(
                 "You managed to control the ship with your skills, but you didnt come out of it unscaved \n You lost: 10 health")
             health = health - 10
         else:
-            print("Wasted!")
+            typingPrint("Wasted!")
             health = health - health
     elif plan == "2":
         difficulty = random.randint(1, 20) + int(skill_mechanic)
         if difficulty >= 15:
-            print(
+            typingPrint(
                 "You managed to perfectly override the engine not putting out to much powa or to little poWAAA")
         elif difficulty >= 10:
-            print("You managed to override the engine but you put to much power in it causing you a major concussion")
+            typingPrint(
+                "You managed to override the engine but you put to much power in it causing you a major concussion")
             health = health - 10
         else:
-            print("you ded")
+            typingPrint("you ded")
     elif plan == "3":
         difficulty = random.randint(1, 20) + int(skill_warfare)
         if difficulty >= 15:
-            print("You shoot the control panel right in the center causing safe mode to activate and saving your sorrow ass. ")
+            typingPrint(
+                "You shoot the control panel right in the center causing safe mode to activate and saving your sorrow ass. ")
             health = health - 50
         elif difficulty >= 10:
-            print("You shoot the control panel right in the center causing the ship to deactivate their thursters leading to ded.")
+            typingPrint(
+                "You shoot the control panel right in the center causing the ship to deactivate their thursters leading to ded.")
             health = health - health
         else:
-            print("you shoot the control panel causing it to explode in you face.")
+            typingPrint(
+                "you shoot the control panel causing it to explode in you face.")
 
 # Loot generator
 
 
 def Loot_event():
-    print("you do be landing tho and locate chest")
+    typingPrint("you do be landing tho and locate chest")
     item_chance = random.randint(1, 100)
 
     if item_chance <= 40:
@@ -136,18 +163,18 @@ def Loot_event():
 #             print("you shoot the control panel causing it to explode in you face.")
 
 
-Start = input("Do you want to play the game y/n? --> ")
+Start = typingInput("Do you want to play the game y/n? --> ")
 
 if Start == ("yes"):
-    print("good")
+    typingPrint("good")
 else:
-    print("Yes you do")
+    typingPrint("Yes you do ")
 
 
-player_name = input("Enter your name here --> ")
+player_name = input("\n Enter your name here --> ")
 
-W = input("Welcome " + player_name +
-          ". We will now randomize your capabilities, press ENTER to continue. -->")
+W = typingInput("Welcome " + player_name +
+                ",\nWe will now randomize your capabilities, press ENTER to continue. -->")
 
 health = 100
 
@@ -155,20 +182,20 @@ skill_pilot = random.randint(7, 8)
 skill_mechanic = random.randint(1, 20)
 skill_warfare = random.randint(1, 20)
 
-print("Your skills are:\n Piloting: " + str(skill_pilot) + "\n mechanical: " +
-      str(skill_mechanic) + "\n Warfare: " + str(skill_warfare) + " ")
+typingPrint("Your skills are:\n Piloting: " + str(skill_pilot) + "\n mechanical: " +
+            str(skill_mechanic) + "\n Warfare: " + str(skill_warfare) + " ")
 
 while health >= 0:
     if health <= 0:
-        print("GAME OVER")
+        typingPrint("GAME OVER")
         break
     else:
         Planet_1 = "Ga"
         Planet_2 = "dagu"
         Planet_3 = "kua"
 
-        planet_choice = input("wich of the following planets would you like to explore for supplies?" "\n [1] " +
-                              Planet_1 + " (habitable) \n [2] " + Planet_2 + " (Terrestial) \n [3] " + Planet_3 + " (desolate rock)\n -->")
+        planet_choice = typingInput("\n\n wich of the following planets would you like to explore for supplies?" "\n [1] " +
+                                    Planet_1 + " (habitable) \n [2] " + Planet_2 + " (Terrestial) \n [3] " + Planet_3 + " (desolate rock)\n [4] Check Inventory  \n [5] Check Stats -->")
 
         if planet_choice == "1":
             Event_pick = random.randint(1, 1)
@@ -176,11 +203,16 @@ while health >= 0:
                 Planet1_event1()
 
         elif planet_choice == "2":
-            print("nej")
+            typingPrint("nej")
 
         elif planet_choice == "3":
-            print("icke")
-        else:
-            print("TRY AGAIN")
+            typingPrint("icke")
 
-print("u mom")
+        elif planet_choice == "4":
+            typingPrint()
+
+        elif planet_choice == "5":
+            typingPrint("\n\n You're current skills are: \n Pilot:" + str(skill_pilot) + "\n Mechanical:" +
+                        str(skill_mechanic) + "\n Warfare:" + str(skill_warfare) + "\n Health:" + str(health) + "\n\n")
+        else:
+            typingPrint("TRY AGAIN")
